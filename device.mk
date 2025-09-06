3c2085e6410b2335110300843d95bcf6095fba01
Deivid21 <david.parra.ignacio@gmail.com>
2025-09-06 01:37:59 -0400

    bangkk: Build MotCamera4 from prebuilt source
    Change-Id: I7a45d3b7db20dff97d64c5d473622bcea30cb44f
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- device.mk
+++ device.mk
@@ -56,6 +56,12 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 $(call soong_config_set_bool,livedisplay_sysfs,enable_af,true)
 
+# Moto Camera 4
+TARGET_MOTCAMERA4 := bangkk
+TARGET_USES_MOTCAMERA4 := true
+
+$(call inherit-product, vendor/motorola/MotCamera4-sm6375/motcamera4.mk)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.sec \
